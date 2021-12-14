80e4c120c7c3ff5ebd70affa595d74f2305ce230
Miguel Ângelo <miguelcg958@gmail.com>
2021-12-14 13:53:03 -0300

    Added plugin custom messages and events feature

--- src/main/java/dev/miguel5g/actionbarannouncer/MessagesManager.java
+++ src/main/java/dev/miguel5g/actionbarannouncer/MessagesManager.java
@@ -6,6 +6,7 @@ import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 
+import java.util.HashMap;
 import java.util.List;
 
 public class MessagesManager {
@@ -15,54 +16,59 @@ public class MessagesManager {
 
     public static MessagesManager instance;
 
-    public List<String> messages;
-    public int interval = 0;
+    public int announcerInterval = 0;
+    public List<String> announcerMessages;
+    public HashMap<String, String> messages = new HashMap<>();
+    public String joinMessage;
+    public String firstJoinMessage;
 
     private MessagesManager(ActionBarAnnouncer plugin) {
         this.plugin = plugin;
         this.plugin.saveDefaultConfig();
         this.loadData();
-        this.setInterval();
+        this.setAnnouncerTask();
     }
 
     private void loadData() {
-        // Get plugin config yaml
         FileConfiguration config = this.plugin.getConfig();
 
-        // Get data from config
-        this.messages = config.getStringList("messages");
-        this.interval = config.getInt("interval", 10);
+        messages.put("no_permission", ChatUtils.parseColors(config.getString("messages.no_permission", "&cYou do not have permission to run this command.")));
+        messages.put("invalid_subcommand", ChatUtils.parseColors(config.getString("messages.invalid_subcommand", "&cIncorrect use! Try to use &4/aba help&c.")));
+        messages.put("reload_success", ChatUtils.parseColors(config.getString("messages.reload_success", "&aSettings reloaded successfully!")));
 
-        // Display the number of messages loaded in the console
-        int messagesLength = this.messages.toArray().length;
+        this.announcerInterval = config.getInt("announcers.interval", 10);
+        this.announcerMessages = config.getStringList("announcers.messages");
+        this.joinMessage = ChatUtils.parseColors(config.getString("player_join.message"));
+        this.firstJoinMessage = ChatUtils.parseColors(config.getString("player_first_join.message"));
+
+        int messagesLength = this.announcerMessages.toArray().length;
+
+        if (this.joinMessage != null) messagesLength++;
+
+        if (this.firstJoinMessage != null) messagesLength++;
 
         ChatUtils.logger("§7Loaded §f" + messagesLength + "§7 messages from config");
     }
 
-    private void setInterval() {
-        // Validate if array length is > 0
-        if (this.messages.toArray().length == 0) return;
+    private void setAnnouncerTask() {
+        if (this.announcerMessages.toArray().length == 0) return;
 
         Runnable executor = () -> {
-            // Check if the current index exists
-            if (this.messages.toArray().length == this.currentIndex) this.currentIndex = 0;
+            if (this.announcerMessages.toArray().length == this.currentIndex) this.currentIndex = 0;
 
-            // Get the raw message and parse to TextComponent
-            String rawMessage = this.messages.get(this.currentIndex);
+            String rawMessage = this.announcerMessages.get(this.currentIndex);
             BaseComponent[] message = TextComponent.fromLegacyText(ChatUtils.parseColors(rawMessage));
 
-            // Get all online players and send the message
-            this.plugin
-                    .getServer()
+            Bukkit
                     .getOnlinePlayers()
                     .forEach((player -> player.spigot().sendMessage(ChatMessageType.ACTION_BAR, message)));
 
-            // Update the current index
             this.currentIndex++;
         };
 
-        // Set te interval task
-        this.taskRef = Bukkit.getScheduler().scheduleSyncRepeatingTask(this.plugin, executor, 0, 20L * this.interval);
+        this.taskRef = Bukkit
+                .getScheduler()
+                .scheduleSyncRepeatingTask(this.plugin, executor, 0, 20L * this.announcerInterval);
     }
 
     public static void init(ActionBarAnnouncer plugin) {
@@ -73,13 +79,14 @@ public class MessagesManager {
         ChatUtils.logger("§7Reloading plugin");
 
         this.currentIndex = 0;
-        this.interval = 0;
-        this.messages = null;
+        this.announcerInterval = 0;
+        this.announcerMessages = null;
 
         Bukkit.getScheduler().cancelTask(this.taskRef);
 
+        this.plugin.saveDefaultConfig();
         this.plugin.reloadConfig();
         this.loadData();
-        this.setInterval();
+        this.setAnnouncerTask();
     }
 }
--- src/main/java/dev/miguel5g/actionbarannouncer/commands/AbaCommand.java
+++ src/main/java/dev/miguel5g/actionbarannouncer/commands/AbaCommand.java
@@ -1,67 +1,44 @@
 package dev.miguel5g.actionbarannouncer.commands;
 
-import dev.miguel5g.actionbarannouncer.ChatUtils;
 import dev.miguel5g.actionbarannouncer.MessagesManager;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class AbaCommand implements CommandExecutor, TabCompleter {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!sender.hasPermission("aba.admin")) {
-            sender.sendMessage("§cVocê não tem permissão para executar este comando.");
+            sender.sendMessage(MessagesManager.instance.messages.get("no_permission"));
             return true;
         }
 
         if (args.length != 1) {
-            sender.sendMessage("§cUso incorreto! Tente utilizar §4/aba help§c.");
+            sender.sendMessage(MessagesManager.instance.messages.get("invalid_subcommand"));
             return true;
         }
 
         if (args[0].equalsIgnoreCase("help")) {
             sender.sendMessage("");
-            sender.sendMessage("§eSubcomandos disponíveis");
+            sender.sendMessage("§eAvailable subcommands");
             sender.sendMessage("");
-            sender.sendMessage(" §6/aba help §7- §eLista de subcomandos.");
-            sender.sendMessage(" §6/aba reload §7- §eRecarrega o plugin.");
-            sender.sendMessage(" §6/aba interval §7- §eIntervalo entre mensagens.");
-            sender.sendMessage(" §6/aba messages §7- §eMensagens carregadas");
+            sender.sendMessage(" §6/aba help §7- §eList of subcommands");
+            sender.sendMessage(" §6/aba reload §7- §eReload settings.");
             sender.sendMessage("");
             return true;
         }
 
         if (args[0].equalsIgnoreCase("reload")) {
             MessagesManager.instance.reload();
-            sender.sendMessage("§aPlugin recarregado com sucesso!");
+            sender.sendMessage(MessagesManager.instance.messages.get("reload_success"));
             return true;
         }
 
-        if (args[0].equalsIgnoreCase("interval")) {
-            sender.sendMessage("§eIntervalo entre as mensagens §6" + MessagesManager.instance.interval + "§e segundos.");
-            return true;
-        }
-
-        if (args[0].equalsIgnoreCase("messages")) {
-            sender.sendMessage("");
-            sender.sendMessage("§e" + MessagesManager.instance.messages.toArray().length + " mensagen(s) carregada(s)");
-            sender.sendMessage("");
-
-            MessagesManager.instance.messages.forEach((message) -> {
-                sender.sendMessage(" §7- §r" + ChatUtils.parseColors(message));
-            });
-
-            sender.sendMessage("");
-            return true;
-        }
-
-        sender.sendMessage("§cUso incorreto! Tente utilizar §4/aba help§c.");
+        sender.sendMessage(MessagesManager.instance.messages.get("invalid_subcommand"));
         return true;
     }
 
@@ -74,8 +51,6 @@ public class AbaCommand implements CommandExecutor, TabCompleter {
 
         completions.add("help");
         completions.add("reload");
-        completions.add("internal");
-        completions.add("messages");
 
         if (args.length == 1) {
             completions.forEach((completion) -> {
