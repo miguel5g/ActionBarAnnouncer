bcf814047e96a3629385ae8b92a60b8aac3f259f
Miguel Ângelo <miguelcg958@gmail.com>
2021-12-13 21:50:54 -0300

    Removing unnecessary fields

--- src/main/java/dev/miguel5g/actionbarannouncer/ActionBarAnnouncer.java
+++ src/main/java/dev/miguel5g/actionbarannouncer/ActionBarAnnouncer.java
@@ -3,14 +3,7 @@ package dev.miguel5g.actionbarannouncer;
 import dev.miguel5g.actionbarannouncer.commands.AbaCommand;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public final class ActionBarAnnouncer extends JavaPlugin {
-    private List<String> messages = new ArrayList<>();
-    private int currentIndex = 0;
-    private int interval = 0;
-
     @Override
     public void onEnable() {
         this.getServer().getConsoleSender().sendMessage(ChatUtils.PREFIX + "§7Plugin enable");
@@ -26,8 +19,4 @@ public final class ActionBarAnnouncer extends JavaPlugin {
     public void onDisable() {
         this.getServer().getConsoleSender().sendMessage(ChatUtils.PREFIX + "§7Plugin disabled");
     }
-
-
-
-
 }
